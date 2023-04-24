c9443115a06a29f2ec9b4f960f5ed05c67ea300f
xsephtion <josephborjaf@gmail.com>
2023-04-24 18:40:13 +0800

    Footer Copyright

--- component/Copyright.tsx
+++ component/Copyright.tsx
@@ -4,7 +4,19 @@ import Typography from "@mui/material/Typography";
 
 export default function Copyright(props: any) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        p: 1,
+        backgroundColor: "background.paper",
+      }}
+      {...props}
+    >
       {"Copyright © "}
       <Link color="inherit" href="">
         Gibraltar
