5047d6c35eeb1afac1f7786a7eb837e5e1a1b7f6
Robby De Leon <robby@sugartech.ph>
2024-02-21 19:52:00 +0800

    Fixed URL Endpoints

--- next.config.js
+++ next.config.js
@@ -1,9 +1,38 @@
 /** @type {import('next').NextConfig} */
+const {networkInterfaces} = require('os')
+
+const getIPAddress = () => {
+  const nets = networkInterfaces()
+  const results = {};
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      if (net.family == "IPv4" && !net.internal) {
+        if (!results[name]) {
+          results[name] = []
+        }
+        results[name].push(net.address)
+      }
+    }
+  }
+
+  const nicNames = Object.keys(results)
+  if (nicNames.length > 0) {
+    const firstNICAddresses = results[nicNames[0]]
+    if (firstNICAddresses.length > 0) {
+      return firstNICAddresses[0]
+    }
+  }
+  return null;
+}
+
+const ipAddress = getIPAddress()
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    //NEXT_API_URL:'http://192.168.68.126:3000'
-    NEXT_API_URL:'http://localhost:3000'
+    NEXT_API_URL:`${ipAddress}:3000`
+    // NEXT_API_URL:'http://localhost:3000'
   },
 }
 
